555f31bc5bbb4fb3121c6da4b2dfaa21522f5c9c
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-10-26 22:50:44 +0300

    fix cmake option name

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 add_compile_options(-Wall -Wextra -Wpedantic -g -fno-omit-frame-pointer)
 
-if(WHEELS_DEVELOPER)
+if(FALLIBLE_DEVELOPER)
     message(STATUS "Turn warnings into errors")
     add_compile_options(-Werror)
 endif()
