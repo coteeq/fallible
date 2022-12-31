38aa060b904bd2e75691b1da63727e8be5d24022
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-12-31 12:34:55 +0300

    integrate with compass

--- fallible/rt/panic.cpp
+++ fallible/rt/panic.cpp
@@ -1,10 +1,23 @@
 #include <fallible/rt/panic.hpp>
 
 #include <fallible/rt/panicker.hpp>
+#include <fallible/rt/abort.hpp>
+
+#include <compass/locate.hpp>
 
 namespace fallible {
 
 namespace rt {
+
+static IPanicker& Panicker() {
+  auto* panicker = compass::Map().TryLocate<IPanicker>();
+
+  if (panicker != nullptr) {
+    return *panicker;
+  } else {
+    return AbortProcess();
+  }
+}
 
 void Panic(const wheels::SourceLocation& where, const std::string& reason) {
   Panicker().Panic(where, reason);
